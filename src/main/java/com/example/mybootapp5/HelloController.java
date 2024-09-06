c6702fd403cbaddbb39fc84851f6d2860c784532
te210816 <tejc210816@gmail.com>
2024-09-06 19:53:23 +0900

    kadai5

---
+++ src/main/java/com/example/mybootapp5/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.mybootapp5;
+//import org.springframework.web.bind.annotation.RestController;
+ 
+import ch.qos.logback.core.model.Model;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * Hello world!
+ */
+@Controller
+public class HelloController 
+{
+@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("msg","this is a setting message");
+		return "index";
+	}
+}
+
+	
