decb2c67977ec877c031a8257caeac596ee46af5
te210816 <tejc210816@gmail.com>
2024-09-10 17:22:59 +0900

    Update HelloController.java

--- src/main/java/com/example/mybootapp5/HelloController.java
+++ src/main/java/com/example/mybootapp5/HelloController.java
@@ -1,21 +1,25 @@
 package com.example.mybootapp5;
-//import org.springframework.web.bind.annotation.RestController;
- 
-import ch.qos.logback.core.model.Model;
- 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-/**
- * Hello world!
- */
-@Controller
-public class HelloController 
-{
-@RequestMapping("/")
-	public String index(Model model) {
-		model.addAttribute("msg","this is a setting message");
-		return "index";
-	}
-}
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model; 
+import org.springframework.web.servlet.ModelAndView;
 
-	
+@Controller
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg", "this is a setting message");
+        return "index";
+    }
+
+    @RequestMapping(value="/post", method=RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam("text1") String text1) {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("msg", "you write '" + text1 + "'!!!");
+        return mv;
+    }
+}
